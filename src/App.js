2f6d6490f62c799a15ad22aa4c43f9e2574d4a34
Federico Ferro <federicomferro@gmail.com>
2022-12-28 21:22:07 -0300

    se crean componentes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import ItemListConteiner from './ItemListConteiner';
 import './App.css';
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+
+<ItemListConteiner />
+
     </div>
   );
 }
---
+++ src/ItemListConteiner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ItemListConteiner = () => {
+  return (
+    <div><h1>Hola, bienvenidos a mi Viento Salvaje</h1>
+    </div>
+  )
+}
+
+export default ItemListConteiner
